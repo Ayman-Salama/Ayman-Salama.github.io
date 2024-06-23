e0470b53600261ab43690bcc76e229f0f7eb3679
Ayman_Salama <ayman@bis-pal.com>
2024-06-23 16:08:17 +0300

    moving on

--- calculator1.js
+++ calculator1.js
@@ -1,7 +1,34 @@
-function calculateSamplerAndEncoder() {
-    var samplingFrequency = document.getElementById('samplingFrequency').value;
-    var numberOfSamples = document.getElementById('numberOfSamples').value;
-    var numberOfBits = samplingFrequency * numberOfSamples; // Simplified calculation
-    var result = 'Number of Bits: ' + numberOfBits; // Example result
-    document.getElementById('result').innerHTML = result;
+function calculateSampler() {
+    var bits = document.getElementById('samplerBits').value;
+    var rate = document.getElementById('samplerRate').value;
+    var output = "Total Bits Transmitted = " + (bits * rate) + " bits/s";
+    document.getElementById('samplerResult').innerHTML = output;
+}
+
+function calculateQuantizer() {
+    var bits = document.getElementById('quantizerBits').value;
+    var rate = document.getElementById('quantizerRate').value;
+    var output = "Total Bits Transmitted = " + (bits * rate) + " bits/s";
+    document.getElementById('quantizerResult').innerHTML = output;
+}
+
+function calculateEncoder() {
+    var bits = document.getElementById('encoderBits').value;
+    var rate = document.getElementById('encoderRate').value;
+    var output = "Total Bits Transmitted = " + (bits * rate) + " bits/s";
+    document.getElementById('encoderResult').innerHTML = output;
+}
+
+function calculateChannelEncoder() {
+    var bits = document.getElementById('channelEncoderBits').value;
+    var rate = document.getElementById('channelEncoderRate').value;
+    var output = "Total Bits Transmitted = " + (bits * rate) + " bits/s";
+    document.getElementById('channelEncoderResult').innerHTML = output;
+}
+
+function calculateInterleaver() {
+    var bits = document.getElementById('interleaverBits').value;
+    var rate = document.getElementById('interleaverRate').value;
+    var output = "Total Bits Transmitted = " + (bits * rate) + " bits/s";
+    document.getElementById('interleaverResult').innerHTML = output;
 }
